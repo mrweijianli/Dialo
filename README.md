dc8c3606aca60a2217ee55ae76a6b16203917278
WEIJIAN LI <weijianli@dhcp-10-105-146-198.wireless.northwestern.private>
2020-03-06 16:08:55 -0600

    doing readme

--- README.md
+++ README.md
@@ -1,2 +1,136 @@
 # Dialo
-Dialo Chit-chat Chatbot 
+
+<!-- ```
+User >>> Hello
+Bot >>> Hello, nice to meet you!
+User >>> Where are you from?
+Bot >>> I'm from England, but I live in the US, USA
+User >>> Which state are you in?
+Bot >>> Alabama, my friend
+User >>> What's the weather there?
+Bot >>> It's cloudy
+``` -->
+
+<!-- Dialo implements 
+  - a decoder ([source]()) for [DialoGPT](https://github.com/microsoft/DialoGPT), 
+  - an interactive multiturn chatbot ([source]()), and 
+  - a Telegram chatbot ([source]()).
+  
+The bot is built around [DialoGPT](https://github.com/microsoft/DialoGPT) - a large-scale pretrained dialogue response generation model trained by Microsoft, which was trained on 147M multi-turn dialogue from Reddit discussion thread. The human evaluation results indicate that its quility is comparable to human response quality under a single-turn conversation Turing test.
+
+Since even with properly filtered Reddit dataset the model can generate toxic/inappropriate responses, the Microsoft team was unable to provide the decoding script. This repository implements the decoding script inspired by `run_generation.py` released earlier by Hugging Face. Moreover, it implements a Telegram bot that can be deployed locally, remotely, and even on Colab, and just makes testing fun. -->
+
+## Problem we tried to solve
+In this project, we intended to develop a contextual chit-chat chatbot that has a user-friendly user interface and is easy to be deployed on any cloud platform.
+
+## Core Model
+This robot is developed based on the model, [DialoGPT](https://github.com/microsoft/DialoGPT), which is based on the language model, [GPT2](https://openai.com/blog/better-language-models/).
+
+## How to run the final deliverable (2 options)
+## Containerized way:
+### 1. Create a Telegram bot
+Please follow this [link](https://tutorials.botsfloor.com/creating-a-bot-using-the-telegram-bot-api-5d3caed3266d) to create a Telegram bot. Please write down the token from the response of Telegram for later use.
+```
+Use this token to access the HTTP API:
+2764xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
+### 2. Clone this repo and enter the repo
+```
+git clone https://github.com/Leevisir/Dialo.git
+cd Dialo
+```
+### 3. Put the API token into the configuration file
+```
+# change this line in the file gpt2bot/chatbot.cfg
+telegram_token = <YOUR_TOKEN_HERE>
+```
+### 4. Build the Docker imgae and run it
+```
+docker build -t dialo .
+docker run dialo
+```
+Now the chatbot is running and you can use Telegram 
+
+### 3. Create a virtual environment and activate it (if anaconda is not installed, please install it first following this [link](https://docs.anaconda.com/anaconda/install/))
+```
+conda create -n dialo python=3.7
+conda activate dialo
+```
+### 4. Install all requirements
+```
+pip install -r requirements.txt
+```
+### 5. 
+  
+## What we did?
+
+### 1. Create a Telegram bot
+
+- Register a new Telegram bot via BotFather (see https://core.telegram.org/bots)
+
+### 2. Deploy the bot (4 options)
+
+#### a. Google Colab
+
+[A Colab interactive notebook](https://colab.research.google.com/drive/1QH9Vq6EEl7lU6Nz7uFHFN5i6rogLaAE2)
+
+A good thing about Google Colab is free GPU. So why not running the Telegram bot there, for blazingly fast chat? Run the notebook at daytime and do not forget to stop it at night.
+
+#### b. Amazon EC2
+<!-- First, search for EC2 in the AWS console search bar:
+![image info](./figures/EC2_search.png)
+On the EC2 Dashboard, launch a new EC2 instance:
+![image info](./figures/launch_instance.png) -->
+First, launch a new EC2 instance
+Select the correct AMI:
+![iamge info](./figures/ubuntu_instance.png)
+Then, select the correct Instance Type with GPU supports. Then one we chose was **p3.2xlarge**  
+After launching the EC2 instance, on the sidebar of the EC2 Dashboard, click **instance**:
+![image info](./figures/ec2_sidebar.png)
+
+
+
+#### c. Amazon ECS
+
+#### d. Local laptop
+
+- Clone the repository
+- Set your parameters such as API token in dialog.cfg
+- To avoid re-downloading model files at each re-deployment, download the model files beforehand with
+```
+# cd gpt2bot/gpt2bot
+python model.py
+```
+- Finally, deploy the container from the root folder
+```
+docker build -t gpt2bot . && docker run gpt2bot
+```
+
+#### Manually
+
+- Clone the repository
+- Set your parameters such as API token in dialog.cfg
+- Install packages listed in requirements.txt
+- Run the script
+```
+# cd gpt2bot/gpt2bot
+python telegram_bot.py
+```
+- To test the things out in the console, run
+```
+python interactive_bot.py
+```
+
+### 3. Start chatting!
+
+![](telegram_bot.gif)
+
+Just start texting. Append @gif for the bot to generate a GIF instead of text. To reset, type "Bye".
+
+
+## References
+
+- [Official DialoGPT implementation](https://github.com/microsoft/DialoGPT) and [DialoGPT paper](https://arxiv.org/abs/1911.00536)
+- [Thread on current decoding scripts](https://github.com/microsoft/DialoGPT/issues/3)
+
+You can wait for a full DialoGPT release and then replace the decoder.
---
+++ gpt2bot/telegram_bot.py
@@ -0,0 +1,226 @@
+#  Copyright (c) polakowo
+#  Licensed under the MIT license.
+
+# !pip install python-telegram-bot --upgrade
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ChatAction, ParseMode
+from functools import wraps
+import configparser
+import argparse
+import logging
+import requests
+from urllib.parse import urlencode
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+import random
+import re
+
+from model import download_model_folder, download_reverse_model_folder, load_model
+from decoder import generate_response
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# https://github.com/python-telegram-bot/python-telegram-bot/wiki/Code-snippets
+
+def start_command(update, context):
+    context.chat_data['turns'] = []
+    update.message.reply_text("Just start texting me. Append \"@gif\" for me to generate a GIF. If I'm getting annoying, type \"Bye\"")
+
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
+
+def translate_message_to_gif(message, config):
+    # https://engineering.giphy.com/contextually-aware-search-giphy-gets-work-specific/
+    params = {
+        'api_key': config.get('chatbot', 'giphy_token'),
+        's': message,
+        'weirdness': config.getint('chatbot', 'giphy_weirdness')
+    }
+    url = "http://api.giphy.com/v1/gifs/translate?" + urlencode(params)
+    response = requests_retry_session().get(url)
+    return response.json()['data']['images']['fixed_height']['url']
+
+def self_decorator(self, func):
+    """Passes bot object to func command."""
+    # TODO: Any other ways to pass variables to handlers?
+    def command_func(update, context, *args, **kwargs):
+        return func(self, update, context, *args, **kwargs)
+    return command_func
+
+def send_action(action):
+    """Sends `action` while processing func command."""
+    def decorator(func):
+        @wraps(func)
+        def command_func(self, update, context, *args, **kwargs):
+            context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=action)
+            return func(self, update, context, *args, **kwargs)
+        return command_func
+    return decorator
+
+send_typing_action = send_action(ChatAction.TYPING)
+
+def gpt_normalize(txt):
+    txt = re.sub(r"[^A-Za-z0-9()\[\]:,.!?'“”\"]", " ", txt) # remove illegal chars
+    return ' '.join(txt.strip().split()) # remove unnecessary spaces
+
+@send_typing_action
+def message(self, update, context):
+    # Parse parameters
+    num_samples = self.config.getint('decoder', 'num_samples')
+    max_turns_history = self.config.getint('decoder', 'max_turns_history')
+    if 'turns' not in context.chat_data:
+        context.chat_data['turns'] = []
+    turns = context.chat_data['turns']
+
+    user_message = update.message.text
+    if user_message.lower() == 'bye':
+        # Restart chat
+        context.chat_data['turns'] = []
+        update.message.reply_text("Bye")
+        return None
+    return_gif = False
+    if '@gif' in user_message:
+        # Return gif
+        return_gif = True
+        user_message = user_message.replace('@gif', '').strip()
+    if max_turns_history == 0:
+        # If you still get different responses then set seed
+        context.chat_data['turns'] = []
+    # A single turn is a group of user messages and bot responses right after
+    turn = {
+        'user_messages': [],
+        'bot_messages': []
+    }
+    turns.append(turn)
+    turn['user_messages'].append(user_message)
+    logger.info(f"{update.effective_message.chat_id} - User >>> {user_message}")
+    # Merge turns into a single history (don't forget EOS token)
+    history = ""
+    from_index = max(len(turns)-max_turns_history-1, 0) if max_turns_history >= 0 else 0
+    for turn in turns[from_index:]:
+        # Each turn begings with user messages
+        for message in turn['user_messages']:
+            history += gpt_normalize(message) + self.tokenizer.eos_token
+        for message in turn['bot_messages']:
+            history += gpt_normalize(message) + self.tokenizer.eos_token
+
+    # Generate bot messages
+    bot_messages = generate_response(
+        self.model, 
+        self.tokenizer, 
+        history, 
+        self.config, 
+        mmi_model=self.mmi_model, 
+        mmi_tokenizer=self.mmi_tokenizer
+    )
+    if num_samples == 1:
+        bot_message = bot_messages[0]
+    else:
+        # TODO: Select a message that is the most appropriate given the context
+        # This way you can avoid loops
+        bot_message = random.choice(bot_messages)
+    turn['bot_messages'].append(bot_message)
+    logger.info(f"{update.effective_message.chat_id} - Bot >>> {bot_message}")
+    if return_gif:
+        # Return response as GIF
+        gif_url = translate_message_to_gif(bot_message, self.config)
+        context.bot.send_animation(update.effective_message.chat_id, gif_url)
+    else:
+        # Return response as text
+        update.message.reply_text(bot_message)
+        
+
+def error(update, context):
+    logger.warning(context.error)
+
+class TelegramBot:
+    def __init__(self, model, tokenizer, config, mmi_model=None, mmi_tokenizer=None):
+        logger.info("Initializing the bot...")
+
+        # Set global variables
+        self.model = model
+        self.tokenizer = tokenizer
+        self.mmi_model = mmi_model
+        self.mmi_tokenizer = mmi_tokenizer
+        self.config = config
+
+        # Set up Telegram bot
+        self.updater = Updater(config.get('chatbot', 'telegram_token'), use_context=True)
+        dp = self.updater.dispatcher
+
+        # on different commands - answer in Telegram
+        # conversation with bot
+        dp.add_handler(MessageHandler(Filters.text, self_decorator(self, message)))
+
+        # chatbot settings
+        dp.add_handler(CommandHandler('start', start_command))
+
+        # log all errors
+        dp.add_error_handler(error)
+
+    def run_chat(self):
+        logger.info("Running the chatbot...")
+
+        # Start the Bot
+        self.updater.start_polling()
+
+        # Run the bot until you press Ctrl-C or the process receives SIGINT,
+        # SIGTERM or SIGABRT. This should be used most of the time, since
+        # start_polling() is non-blocking and will stop the bot gracefully.
+        self.updater.idle()
+
+def main():
+    # Script arguments can include path of the config
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--config', type=str, default="chatbot.cfg")
+    args = arg_parser.parse_args()
+
+    # Read the config
+    config = configparser.ConfigParser(allow_no_value=True)
+    with open(args.config) as f:
+        config.read_file(f)
+
+    # Download and load main model
+    target_folder_name = download_model_folder(config)
+    # added by Weijian, avoid re-downloading the model
+    # data_folder = config.get('model', 'data_folder')
+    # model_size = config.get('model', 'model_size')
+    # dataset = config.get('model', 'dataset')
+    # from_scratch = config.getboolean('model', 'from_scratch')
+    # target_folder_name = model_size + "_" + dataset + ("_fs" if from_scratch else "_ft")
+    model, tokenizer = load_model(target_folder_name, config)
+
+    # Download and load reverse model
+    use_mmi = config.getboolean('model', 'use_mmi')
+    if use_mmi:
+        mmi_target_folder_name = download_reverse_model_folder(config)
+        mmi_model, mmi_tokenizer = load_model(mmi_target_folder_name, config)
+    else:
+        mmi_model = None
+        mmi_tokenizer = None
+    
+    # Run Telegram bot
+    bot = TelegramBot(model, tokenizer, config, mmi_model=mmi_model, mmi_tokenizer=mmi_tokenizer)
+    bot.run_chat()
+    
+if __name__ == '__main__':
+    main()
